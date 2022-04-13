386e671b673af1fc4670adf2eae36dc4cba99724
nmpereira <nikhilpereira25@gmail.com>
2022-04-13 01:05:41 -0400

    inital files

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3005;
+const mongoose = require("mongoose");
+mongoose.connect(process.env.db_devdash);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.error("Connected to db"));
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send("users");
+});
